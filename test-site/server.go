18adddc487cdcc4cc0201f6b2d68d8e965036161
Katy248 <petrovanton247@gmail.com>
2024-05-02 14:26:57 +0300

    Update server

--- test-site/server.go
+++ test-site/server.go
@@ -1,8 +1,11 @@
 package main
 
 import (
-	"log"
+	// "log"
 	"net/http"
+
+	// "github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
 
 func staticFileHandler(w http.ResponseWriter, r *http.Request) {
@@ -20,11 +23,46 @@ func staticFileHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	log.Println("Init")
-	http.Handle("/", http.FileServer(http.Dir("./out")))
-	http.HandleFunc("/test", staticFileHandler)
+	router := gin.Default()
+
+	/* corsConfig := cors.Config{
+		AllowMethods: []string{"POST", "GET"},
+		// AllowHeaders:     []string{"*"},
+		// ExposeHeaders:    []string{"*"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return true
+		},
+		// MaxAge: 12 * time.Hour,
+	} */
+
+	router.Use(CORSMiddleware())
+
+	router.StaticFS("/", http.Dir("out"))
+
+	router.Run(":1488")
+
+	/* http.Handle("/", http.FileServer(http.Dir("./out")))
+	http.HandleFunc("/test", CORS(staticFileHandler))
 	log.Println("Starting server at port 1488")
 	if err := http.ListenAndServe(":1488", nil); err != nil {
 		log.Fatal(err)
+	} */
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
 	}
 }
